d28ff5dcc376a782b48a804a252b9c1bff608490
Ramez Emad <120984487+meeeeeeeeezo@users.noreply.github.com>
2022-12-22 15:40:50 +0200

    create an AppBar

--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/shared/colors.dart';
 
 class Details extends StatelessWidget {
   const Details({super.key});
@@ -6,7 +7,40 @@ class Details extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("details"),
-    );
+        appBar: AppBar(
+          actions: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                        child: Text(
+                          "1",
+                          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                        ),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(211, 164, 255, 193),
+                            shape: BoxShape.circle)),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.add_shopping_cart),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Text(
+                    "\$ 30,000",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          ],
+          backgroundColor: appbarGreen,
+          title: Text("Home"),
+        ),
+        body: Text("details"));
   }
 }
