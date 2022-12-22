7c15422227575f9b245e5a43d1bc22f3f42a365e
Ramez Emad <120984487+meeeeeeeeezo@users.noreply.github.com>
2022-12-22 15:04:58 +0200

    change prices for items

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,14 +13,14 @@ class Item {
 
 class Home extends StatelessWidget {
   final List<Item> items = [
-    Item(imgPath: "assets/img/Audi.png", price: 15000),
-    Item(imgPath: "assets/img/car.png", price: 15000),
-    Item(imgPath: "assets/img/hyundai.png", price: 15000),
-    Item(imgPath: "assets/img/kia selots.png", price: 15000),
-    Item(imgPath: "assets/img/Nissan.png", price: 15000),
-    Item(imgPath: "assets/img/volvo.png", price: 15000),
-    Item(imgPath: "assets/img/volvo.png", price: 15000),
-    Item(imgPath: "assets/img/kia.png", price: 15000),
+    Item(price: 15000, imgPath: "assets/img/Audi.png"),
+    Item(price: 30000, imgPath: "assets/img/w.png"),
+    Item(price: 22000, imgPath: "assets/img/hyundai.png"),
+    Item(price: 18000, imgPath: "assets/img/kia selots.png"),
+    Item(price: 20000, imgPath: "assets/img/Nissan.png"),
+    Item(price: 32000, imgPath: "assets/img/volvo.png"),
+    Item(price: 28000, imgPath: "assets/img/volvo.png"),
+    Item(price: 15000, imgPath: "assets/img/kia.png"),
   ];
 
   Home({Key? key}) : super(key: key);
