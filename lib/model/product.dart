b0668621a792f06fee0c6c4cd21de1b61fc83ac7
Ramez Emad <120984487+meeeeeeeeezo@users.noreply.github.com>
2022-12-22 15:24:10 +0200

    put that class in new file

---
+++ lib/model/product.dart
@@ -0,0 +1,16 @@
+class Item {
+  String imgPath;
+  double price;
+  Item({required this.imgPath, required this.price});
+}
+
+final List<Item> items = [
+  Item(price: 15000, imgPath: "assets/img/Audi.png"),
+  Item(price: 30000, imgPath: "assets/img/w.png"),
+  Item(price: 22000, imgPath: "assets/img/hyundai.png"),
+  Item(price: 18000, imgPath: "assets/img/kia selots.png"),
+  Item(price: 20000, imgPath: "assets/img/Nissan.png"),
+  Item(price: 32000, imgPath: "assets/img/volvo.png"),
+  Item(price: 28000, imgPath: "assets/img/volvo.png"),
+  Item(price: 15000, imgPath: "assets/img/kia.png"),
+];
