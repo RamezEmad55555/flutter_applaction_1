f8c51fb509d05ae3e7c26796b53fd61ecb49427a
Ramez Emad <120984487+meeeeeeeeezo@users.noreply.github.com>
2022-12-23 14:48:08 +0200

    And return the Text

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,132 +14,139 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.only(top: 12),
-            child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 3 / 2.5,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 33),
-                itemCount: items.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Details(product: items[index]),
-                        ),
-                      );
-                    },
-                    child: GridTile(
-                      child: Stack(children: [
-                        Positioned(
-                          top: -3,
-                          bottom: -9,
-                          right: 0,
-                          left: 0,
-                          child: ClipRRect(
-                              borderRadius: BorderRadius.circular(55),
-                              child: Image.asset(items[index].imgPath)),
-                        ),
-                      ]),
-                      footer: GridTileBar(
-                        trailing: IconButton(
-                            color: Color.fromARGB(255, 62, 94, 70),
-                            onPressed: () {},
-                            icon: Icon(Icons.add)),
-                        leading: Text("\$ 14,000"),
-                        title: Text(
-                          "",
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 0, 0, 0),
-                              fontSize: 20),
+        child: Scaffold(
+            body: Padding(
+              padding: const EdgeInsets.only(top: 12),
+              child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 3 / 2.5,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 33),
+                  itemCount: items.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                Details(product: items[index]),
+                          ),
+                        );
+                      },
+                      child: GridTile(
+                        child: Stack(children: [
+                          Positioned(
+                            top: -3,
+                            bottom: -9,
+                            right: 0,
+                            left: 0,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(55),
+                                child: Image.asset(items[index].imgPath)),
+                          ),
+                        ]),
+                        footer: GridTileBar(
+                          trailing: IconButton(
+                              color: Color.fromARGB(255, 62, 94, 70),
+                              onPressed: () {},
+                              icon: Icon(Icons.add)),
+                          leading: Text("\$ 14,000"),
+                          title: Text(
+                            "",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 0, 0, 0),
+                                fontSize: 20),
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                }),
-          ),
-          drawer: Drawer(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    UserAccountsDrawerHeader(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage("assets/img/car.png"),
-                            fit: BoxFit.cover),
-                      ),
-                      currentAccountPicture: CircleAvatar(
-                          radius: 55,
-                          backgroundImage: AssetImage("assets/img/me.png")),
-                      accountEmail: Text("Ramezemad@gmail.com"),
-                      accountName: Text("Ramez Emad",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255),
-                          )),
-                    ),
-                    ListTile(
-                        title: Text("Home"),
-                        leading: Icon(Icons.home),
-                        onTap: () {}),
-                    ListTile(
-                        title: Text("My Products"),
-                        leading: Icon(Icons.add_shopping_cart),
-                        onTap: () {}),
-                    ListTile(
-                        title: Text("About"),
-                        leading: Icon(Icons.help_center),
-                        onTap: () {}),
-                    ListTile(
-                        title: Text("Logout"),
-                        leading: Icon(Icons.exit_to_app),
-                        onTap: () {}),
-                  ],
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 12),
-                  child: Text("Developed by Ramez Emad © 2023",
-                      style: TextStyle(fontSize: 13)),
-                )
-              ],
+                    );
+                  }),
             ),
-          ),
-          appBar: AppBar(actions: [
-            Row(
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                        child: Text(
-                          "0",
-                          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-                        ),
-                        padding: EdgeInsets.all(5),
+            drawer: Drawer(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      UserAccountsDrawerHeader(
                         decoration: BoxDecoration(
-                            color: Color.fromARGB(211, 164, 255, 193),
-                            shape: BoxShape.circle)),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.add_shopping_cart),
+                          image: DecorationImage(
+                              image: AssetImage("assets/img/car.png"),
+                              fit: BoxFit.cover),
+                        ),
+                        currentAccountPicture: CircleAvatar(
+                            radius: 55,
+                            backgroundImage: AssetImage("assets/img/me.png")),
+                        accountEmail: Text("Ramezemad@gmail.com"),
+                        accountName: Text("Ramez Emad",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            )),
+                      ),
+                      ListTile(
+                          title: Text("Home"),
+                          leading: Icon(Icons.home),
+                          onTap: () {}),
+                      ListTile(
+                          title: Text("My Products"),
+                          leading: Icon(Icons.add_shopping_cart),
+                          onTap: () {}),
+                      ListTile(
+                          title: Text("About"),
+                          leading: Icon(Icons.help_center),
+                          onTap: () {}),
+                      ListTile(
+                          title: Text("Logout"),
+                          leading: Icon(Icons.exit_to_app),
+                          onTap: () {}),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 12),
+                    child: Text("Developed by Ramez Emad © 2023",
+                        style: TextStyle(fontSize: 13)),
+                  )
+                ],
+              ),
+            ),
+            appBar: AppBar(
+              actions: [
+                Row(
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                            child: Text(
+                              "0",
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 0, 0, 0)),
+                            ),
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                color: Color.fromARGB(211, 164, 255, 193),
+                                shape: BoxShape.circle)),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.add_shopping_cart),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Text(
+                        "\$ 0",
+                        style: TextStyle(fontSize: 20),
+                      ),
                     ),
                   ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 10),
-                  child: Text(
-                    "\$ 0",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
               ],
-            ),
-          ], backgroundColor: appbarGreen, title: Consumer<Cart>())),
-    );
+              backgroundColor: appbarGreen,
+              title: Consumer<Cart>(builder: ((context, classInstancee, child) {
+                return Text("${classInstancee.myName}");
+              })),
+            )));
   }
 }
