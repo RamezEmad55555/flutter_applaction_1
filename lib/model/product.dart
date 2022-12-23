746a475c4b8144b9f7143fef71b34c97b1f1863c
Ramez Emad <120984487+meeeeeeeeezo@users.noreply.github.com>
2022-12-23 23:00:08 +0200

    adding the name to the productes

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -1,16 +1,17 @@
 class Item {
   String imgPath;
   double price;
-  Item({required this.imgPath, required this.price});
+  String name;
+  Item({required this.imgPath, required this.name, required this.price});
 }
 
 final List<Item> items = [
-  Item(price: 15000, imgPath: "assets/img/Audi.png"),
-  Item(price: 30000, imgPath: "assets/img/w.png"),
-  Item(price: 22000, imgPath: "assets/img/hyundai.png"),
-  Item(price: 18000, imgPath: "assets/img/kia selots.png"),
-  Item(price: 20000, imgPath: "assets/img/Nissan.png"),
-  Item(price: 32000, imgPath: "assets/img/volvo.png"),
-  Item(price: 28000, imgPath: "assets/img/volvo.png"),
-  Item(price: 15000, imgPath: "assets/img/kia.png"),
+  Item(name: "Audi", price: 15000, imgPath: "assets/img/Audi.png"),
+  Item(name: "Volex", price: 30000, imgPath: "assets/img/w.png"),
+  Item(name: "Hyundai", price: 22000, imgPath: "assets/img/hyundai.png"),
+  Item(name: "Kia selots", price: 18000, imgPath: "assets/img/kia selots.png"),
+  Item(name: "Nissan", price: 20000, imgPath: "assets/img/Nissan.png"),
+  Item(name: "Volvo 2021", price: 32000, imgPath: "assets/img/volvo.png"),
+  Item(name: "Volvo 2019", price: 28000, imgPath: "assets/img/volvo.png"),
+  Item(name: "Kia", price: 15000, imgPath: "assets/img/kia.png"),
 ];
