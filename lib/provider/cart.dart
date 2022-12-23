1bf094def9ad38645c704145674464f356397769
Ramez Emad <120984487+meeeeeeeeezo@users.noreply.github.com>
2022-12-23 16:32:05 +0200

    apper the price side for the cart

--- lib/provider/cart.dart
+++ lib/provider/cart.dart
@@ -3,6 +3,7 @@ import 'package:flutter_application_1/model/product.dart';
 
 class Cart with ChangeNotifier {
   List selectedProducts = [];
+  double price = 0;
 
   add(Item product) {
     selectedProducts.add(product);
