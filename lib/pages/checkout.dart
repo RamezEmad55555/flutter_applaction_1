80d3f65d6b5143973a751333b511fa6e417fa39e
Ramez Emad <120984487+meeeeeeeeezo@users.noreply.github.com>
2022-12-23 17:47:02 +0200

    make a statelesswidget

---
+++ lib/pages/checkout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/shared/appbar.dart';
+import 'package:provider/provider.dart';
+
+class CheckOut extends StatelessWidget {
+  const CheckOut({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return;
+  }
+}
