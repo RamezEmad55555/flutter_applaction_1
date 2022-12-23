8c4067d24b818fc5bc454563fec2b5c93a0014f8
Ramez Emad <120984487+meeeeeeeeezo@users.noreply.github.com>
2022-12-23 15:35:31 +0200

    add the products in this list

--- lib/provider/cart.dart
+++ lib/provider/cart.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/model/product.dart';
 
 class Cart with ChangeNotifier {
   List selectedProducts = [];
+
+  add(Item product) {
+    selectedProducts.add(product);
+  }
 }
